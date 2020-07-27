564209b4f45c86b62a63edabc48ca471bf233bfe
Myroslav Stelmashchuk <myroslav.stel@coaxsoft.com>
2020-07-27 13:30:40 +0300

    remove db dependency gtom core

--- app/emails/mailProvider.js
+++
@@ -1,5 +0,0 @@
-const mailjet = require('node-mailjet').connect(process.env.MAILJET_PUBLIC_KEY, process.env.MAILJET_PRIVATE_KEY);
-
-module.exports = {
-  mailjet
-};
--- app/middleware/expressValidation/authValidation/loginSchema.js
+++
@@ -1,6 +0,0 @@
-const { checkSchema } = require('express-validator');
-const { email } = require("../general");
-
-module.exports = checkSchema({
-  email
-});
