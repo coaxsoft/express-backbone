7201dba45459356940f088aa6ec24c11e6695d5c
Myroslav Stelmashchuk <myroslav.stel@coaxsoft.com>
2020-07-28 15:15:36 +0300

    add script to fetch core core

--- fetchCore.sh
+++ fetchCore.sh
@@ -1,1 +1,15 @@
-curl -L https://codeload.github.com/Myrko-SM/express-backbone/zip/core_dev | tar zx
+coreBranch="core"
+
+curl -L https://codeload.github.com/Myrko-SM/express-backbone/zip/"$coreBranch" | tar zx
+
+cp express-backbone-"$coreBranch"/app/app.js app/app.js
+cp express-backbone-"$coreBranch"/package.json package.json
+mkdir "bin"
+cp express-backbone-"$coreBranch"/bin/www bin/www
+cp express-backbone-"$coreBranch"/.env.example .env.example
+
+rm -rf express-backbone-"$coreBranch"
+
+npm install
+
+npm i --save $(<packages.txt)
