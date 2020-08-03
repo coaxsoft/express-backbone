c67407957b2b1834f04cb36c032d180e9d0b1d40
Myroslav Stelmashchuk <myroslav.stel@coaxsoft.com>
2020-08-03 17:35:01 +0300

    fix core test

--- test/core/basic.test.js
+++ test/core/basic.test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const should = chai.should();
 const expect = chai.expect;
 const chaiHttp = require("chai-http");
-const server = require("../app/app");
+const server = require("../../app/app");
 
 chai.use(chaiHttp);
 
